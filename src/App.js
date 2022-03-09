fd3c8687400040dd6f409bb930ce959c914fe6a4
IMRANDIL <aliimranadil2@gmail.com>
2022-03-09 19:17:07 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Stepper from "./components/Stepper";
+import StepperControl from "./components/StepperControl";
 
 function App() {
   return (
-    <div className="text-3xl">
-      hi ali
+    <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-white">
+      {/* stepper */}
+      <div className="container horizontal mt-5">
+        <Stepper />
+
+      </div>
+
+
+
+      {/* navigation controls */}
+
+      <StepperControl />
+
     </div>
   );
 }
---
+++ src/components/Stepper.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Stepper = () => {
+    return (
+        <div className='mx-4 p-4 flex justify-between items-center'>
+            <div>
+                {/* display number */}
+
+
+
+                {/* display description */}
+
+            </div>
+            <div>
+                {/* display line */}
+            </div>
+        </div>
+    )
+}
+
+export default Stepper
