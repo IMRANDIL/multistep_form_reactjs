ae472883613451dc87b60ae6ad76d9e1fe519d63
IMRANDIL <aliimranadil2@gmail.com>
2022-03-10 16:56:00 +0530

    yh

--- src/components/StepperControl.jsx
+++ src/components/StepperControl.jsx
@@ -20,7 +20,7 @@ const StepperControl = ({ steps, currentStep, handleClick }) => {
 
             <button
                 onClick={() => handleClick('next')}
-                className='bg-green-500 text-white uppercase py-2 px-4 rounded-xl font-semibold cursor-pointer  hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out'>
+                className={`bg-green-500 text-white uppercase py-2 px-4 rounded-xl font-semibold cursor-pointer  hover:bg-slate-700 hover:text-white transition duration-200 ease-in-out ${currentStep === steps.length ? 'opacity-50 cursor-not-allowed' : ''}`}>
 
                 {currentStep === steps.length - 1 ? 'Confirm' : 'Next'}
             </button>
